03b34a20171cb12fbe9400a286fed7008cfbe355
Anthony Cera <ace_cera@yahoo.com>
2018-03-02 22:58:01 -0800

    added friends array and module.exports.

---
+++ app/data/friends.js
@@ -0,0 +1,23 @@
+const friends = [{
+    name: "Thor",
+    photo: "link",
+    score: [5, 3, 1, 1, 3, 2, 5, 4, 5, 3]
+}, {
+    name: "Loki",
+    photo: "link",
+    score: [4, 3, 3, 2, 5, 4, 1, 1, 2, 2]
+}, {
+    name: "Valkyrie",
+    photo: "link",
+    score: [2, 2, 1, 3, 4, 3, 5, 5, 1, 1]
+}, {
+    name: "Hela",
+    photo: "link", 
+    score: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+}, {
+    name: "Heimdall",
+    photo: "link",
+    score: [5, 4, 5, 3, 2, 4, 2, 1, 3, 4]
+}];
+
+module.exports = friends;
