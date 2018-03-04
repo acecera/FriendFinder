79eaf545454e94a9eb8313c7ce7e1831281abb9e
Anthony Cera <ace_cera@yahoo.com>
2018-03-04 15:10:59 -0800

    added links for photos.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,22 +1,22 @@
 const friends = [{
     name: "Thor",
-    photo: "link",
+    photo: "https://media.giphy.com/media/3ohhweiVB36rAlqVCE/giphy.gif",
     score: [5, 3, 1, 1, 3, 2, 5, 4, 5, 3]
 }, {
     name: "Loki",
-    photo: "link",
+    photo: "https://media.giphy.com/media/dzKU9L98kJSLK/giphy.gif",
     score: [4, 3, 3, 2, 5, 4, 1, 1, 2, 2]
 }, {
     name: "Valkyrie",
-    photo: "link",
+    photo: "https://media.giphy.com/media/xTkcELz3rjpLDDRAOI/giphy.gif",
     score: [2, 2, 1, 3, 4, 3, 5, 5, 1, 1]
 }, {
     name: "Hela",
-    photo: "link", 
+    photo: "https://media.giphy.com/media/amVrubwAybwrK/giphy.gif", 
     score: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
 }, {
     name: "Heimdall",
-    photo: "link",
+    photo: "https://media.giphy.com/media/OL7qroU4YhkB2/giphy.gif",
     score: [5, 4, 5, 3, 2, 4, 2, 1, 3, 4]
 }];
 
