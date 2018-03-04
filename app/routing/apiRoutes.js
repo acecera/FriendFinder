7f59c2a6205ad9fcd5eda0a975334f29f2cf8847
Anthony Cera <ace_cera@yahoo.com>
2018-03-03 21:49:09 -0800

    fixed variable names and for loop syntax and took out if statement.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,7 +10,20 @@ module.exports = function (app) {
     app.post("/api/friends", function(req, res) {
         var friends = req.body;
         var Score = friends.score;
-        var userScore = 
-    })
+        var userScore = [j];
+        var bestMatch = 100;
+
+        for (var i = 0; i < friends.length; i++) {
+            for (var j = 0; j < score.length; j++) {
+                totalDifference += Math.abs( userScore[j] - friends[i].score[j]);
+            }
+            if (totalDifference < bestMatch) {
+                
+            }
+        }
+        console.log(totalDifference);
+        console.log("Best Match: " + friends[i]);
+        res.json(friends[i]);
+    });
 };
 
